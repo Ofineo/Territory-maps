65bad0b1754b1c7957e8b76d5b0127e1857935a2
Jordi <ofineo@gmail.com>
2018-04-08 19:49:50 +0100

    feature add center pan to device location

--- src/App.js
+++ src/App.js
@@ -3,11 +3,34 @@ import './App.css';
 import Map from './Map';
 
 class App extends Component {
+
+  state = {
+  }
+
+  setMapCenter = (ref) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        console.log(ref);
+        ref.panTo(center);
+      });
+    } else {
+      // Browser doesn't support Geolocation
+    }
+  }
+
   render() {
     return (
       <div>Welcome to Map It
-        <Map/>
-        
+        <Map
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100vh` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          mapReference={(ref) => this.setMapCenter(ref)}
+        />
       </div>
     );
   }
